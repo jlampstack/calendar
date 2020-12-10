428fafe04a2e461e0c729335ee30e53a7e4ba300
Jay Lampstack <jlampstack@gmail.com>
2020-12-10 14:47:42 -0500

    Added event handlers to arrows to display renderCalendar dynamically

--- date.js
+++ date.js
@@ -1,6 +1,4 @@
-///////////////////
-// - DATE LISTS
-//////////////////
+// ========== Date Lists ========== //
 
 // Feb is only month that changes in number of days
 export const months = [
@@ -62,103 +60,37 @@ export const daysShort = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 export const daysLetter = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 
-//////////////////////
-// - CURRENT DATES
-/////////////////////
+// ========== Set Date ========== //
 
-// SET current date
 export const d = new Date();
-// Current Year
-export const currYear = d.getFullYear();
-// Current Month
-export const currMonthIndex = d.getMonth();
-export const currMonth = months[d.getMonth()];
-export const currMonthShort = monthsShort[d.getMonth()];
-export const currMonthLetter = monthsLetter[d.getMonth()];
-export const currMonthFirstDate = new Date(d.getFullYear(), d.getMonth(), 1);
-export const currMonthLastDay = new Date(
-	d.getFullYear(),
-	d.getMonth() + 1,
-	0,
-).getDate();
-export const currMonthOffsetStart = getOffsetStart(
-	d.getFullYear(),
-	d.getMonth(),
-);
-export const currMonthOffsetEnd = getOffsetEnd(d.getFullYear(), d.getMonth());
-// Previous Month
-export const prevMonthLastDay = new Date(
-	d.getFullYear(),
-	d.getMonth(),
-	0,
-).getDate();
-// Day
-export const currDayIndex = d.getDay();
-export const currDay = days[d.getDay()];
-export const currDayShort = daysShort[d.getDay()];
-export const currDayLetter = daysLetter[d.getDay()];
 
-///////////////////////
-// - DATE FUNCTIONS
-//////////////////////
+// ========== Date Functions ========== //
 
-/**
- * Get number of days in a month
- *
- * @param   {number}  year - target year
- * @param   {number}  month - target month 0 to 11 (Jan - Dec)
- *
- * @return  {number}  last day of  targeted month = num days in month
- */
-export function getNumDaysInMonth(year, month) {
+// Get last date of month, also same as the number of dates a month has
+export function getLastDateOfMonth() {
 	// First date of next month - 1
-	return new Date(year, month + 1, 0).getDate();
+	return new Date(d.getFullYear(), d.getMonth() + 1, 0).getDate();
 }
 
-/**
- * GET offset for the start of the month.
- *
- * Number of indexes to add to the start of the calendar month.
- * Represents dates from the previous month to make the row even.
-
- *
- * @return  {number}
- */
-export function getOffsetStart(year, month) {
-	return new Date(year, month, 1).getDay();
+// Get offset for the start of the month
+export function getOffsetStart() {
+	return new Date(d.getFullYear(), d.getMonth(), 1).getDay();
 }
 
-/**
- * GET offset for the end of the month.
- *
- * Number of indexes to add to the end of the calendar month.
- * Represents dates from the next month to make the row even.
- *
- * @return  {number}
- */
+// Get offset for the end of the month
 export function getOffsetEnd() {
 	let lastDayIndex = new Date(d.getFullYear(), d.getMonth() + 1, 1).getDay();
 	return 6 - lastDayIndex + 1;
 }
 
-/**
- * Get the index of the first day of the month
- *
- * @param   {number}  year - target year
- * @param   {number}  month - target month 0 to 11 (Jan - Dec)
- * @return  {number}  first day index of month
- */
+// Get day index (0 - 6) of the first date of the month
 export function getFirstDayIndex(year, month) {
 	return new Date(year, month).getDay();
 }
 
-/**
- * Find out if it's a leapyear
- *
- * @param   {number}  year  - target year
- *
- * @return  {bool}
- */
-export function isLeapYear(year) {
-	return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
+// Find out if it's a leapyear
+export function isLeapYear() {
+	return d.getFullYear() % 100 === 0
+		? d.getFullYear() % 400 === 0
+		: d.getFullYear() % 4 === 0;
 }
--- script.js
+++ script.js
@@ -1,23 +1,14 @@
 import {
 	d,
 	months,
-	currYear,
-	currMonthIndex,
-	currMonth,
-	currMonthShort,
-	currMonthLetter,
-	currMonthFirstDate,
-	currDayIndex,
-	currDay,
-	currDayShort,
-	currDayLetter,
-	currMonthLastDay,
-	prevMonthLastDay,
-	currMonthOffsetStart,
-	currMonthOffsetEnd,
+	getOffsetStart,
+	getLastDateOfMonth,
+	isLeapYear,
 } from './date.js';
 
-// ==========  Elements ========== //
+console.log(d);
+
+// ==========  Global Elements ========== //
 
 // Query Alias
 const query = document.querySelector.bind(document);
@@ -25,56 +16,72 @@ const queryAll = document.querySelectorAll.bind(document);
 
 // Elements
 const datesContainer = query('.calendar_dates');
+const prevArrow = query('.arrow_prev-month');
+const nextArrow = query('.arrow_next-month');
 const monthHeading = query('.calendar_month');
-monthHeading.innerText = `${currMonth} ${currYear}`;
 
-// Returns 5 or 6 rows depending on month
-const numRows = currMonthOffsetStart + currMonthLastDay > 35 ? 6 : 5;
-const numDatesTotal = numRows * 7;
+// Renders the calendar
+function renderCalendar() {
+	// Month heading
+	monthHeading.innerText = `${months[d.getMonth()]} ${d.getFullYear()}`;
 
-// Add appropriate num of rows (5 or 6)
-for (let row = 0; row < numRows; row++) {
-	datesContainer.innerHTML += `<div class="row"></row>`;
+	// Returns 5 or 6 rows depending on month
+	const numRows = getOffsetStart() + getLastDateOfMonth() > 35 ? 6 : 5;
+	const numDatesTotal = numRows * 7;
+
+	// Important: resets html evertime an event is fired
+	datesContainer.innerHTML = ``;
+
+	// Add appropriate num of rows (5 or 6)
+	for (let row = 0; row < numRows; row++) {
+		datesContainer.innerHTML += `<div class="row"></row>`;
+	}
+
+	// Define after rows are added to the dom
+	const rowCollection = queryAll('.row');
+
+	// Add 7 dates to dach row
+	rowCollection.forEach(row => {
+		for (let i = 0; i < 7; i++) {
+			row.innerHTML += `<div class="date">loading...</div>`;
+		}
+	});
+
+	// Define after dates are added to the dom
+	const dateCollection = queryAll('.date');
+
+	// Add dates
+	for (let x = 0; x < numRows * 7; x++) {
+		// Starts at the first index
+		let date = new Date(
+			d.getFullYear(),
+			d.getMonth(),
+			1 - getOffsetStart() + x,
+		);
+
+		// If current month add date base class for basic styling
+		if (date.getMonth() === d.getMonth()) {
+			dateCollection[
+				x
+			].innerHTML = `<div class="date_num">${date.getDate()}</div>`;
+		} else {
+			// Add class for special styling to dates not of current month
+			dateCollection[
+				x
+			].innerHTML = `<div class="date_num date_num--blur">${date.getDate()}</div>`;
+			dateCollection[x].classList.add('date--blur');
+		}
+	}
 }
 
-// Define after rows are added to the dom
-const rowCollection = queryAll('.row');
+renderCalendar();
 
-// Add 7 dates to dach row
-rowCollection.forEach(row => {
-	for (let i = 0; i < 7; i++) {
-		row.innerHTML += `<div class="date">loading...</div>`;
-	}
+prevArrow.addEventListener('click', e => {
+	d.setMonth(d.getMonth() - 1);
+	renderCalendar();
 });
 
-// Define after dates are added to the dom
-const dateCollection = queryAll('.date');
-
-// Add dates
-for (let x = 0; x < numRows * 7; x++) {
-	// Starts at the first index
-	let date = new Date(
-		d.getFullYear(),
-		d.getMonth(),
-		1 - currMonthOffsetStart + x,
-	);
-
-	// If current month add date base class for basic styling
-	if (date.getMonth() === d.getMonth()) {
-		dateCollection[
-			x
-		].innerHTML = `<div class="date_num">${date.getDate()}</div>`;
-	} else {
-		// Add class for special styling to dates not of current month
-		dateCollection[
-			x
-		].innerHTML = `<div class="date_num date_num--blur">${date.getDate()}</div>`;
-		dateCollection[x].classList.add('date--blur');
-	}
-
-	// Add focus to current date if date is "Today", toDateString removes timestamp
-	if (date.toDateString() === d.toDateString()) {
-		// The date number is the first child node
-		dateCollection[x].childNodes[0].classList.add('today');
-	}
-}
+nextArrow.addEventListener('click', e => {
+	d.setMonth(d.getMonth() + 1);
+	renderCalendar();
+});
